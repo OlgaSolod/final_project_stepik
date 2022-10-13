8bc31ab3631795212a82f46c3ee89ef765fe5b6e
Olga Solodko <jasminess847@gmail.com>
2022-10-13 22:27:21 +0300

    refactoring test_main_page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    page = MainPage(browser, link)
+    page.open()
     go_to_login_page(browser)
 
 def go_to_login_page(browser):
